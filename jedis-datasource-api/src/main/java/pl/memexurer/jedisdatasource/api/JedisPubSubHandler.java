4e689c11ddd3f2a5235d6f2bdccff3b55c3d6a03
Memexurer <{meme}+xurer@testzeber.pl>
2022-04-25 18:56:05 +0200

    Add support for old versions

--- jedis-datasource-api/src/main/java/pl/memexurer/jedisdatasource/api/JedisPubSubHandler.java
+++ jedis-datasource-api/src/main/java/pl/memexurer/jedisdatasource/api/JedisPubSubHandler.java
@@ -1,5 +1,11 @@
 package pl.memexurer.jedisdatasource.api;
 
 public interface JedisPubSubHandler {
-  void handle(String channel, byte[] message);
+  default void handle(String channel, byte[] message) {
+    handle(channel, new String(message));
+  }
+
+  default void handle(String channel, String message) {
+    throw new UnsupportedOperationException();
+  }
 }
